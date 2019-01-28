e93a9952369542db2db627eb1005d58255a67f89
awu <awu@cortex.net>
2019-01-27 22:10:15 -0700

    add context

---
+++ daltest/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using dal;
+using Xunit;
+
+namespace daltest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            using (var ctxt = new ScmContext())
+            {
+                var partName = "Sample" + DateTime.Now.ToString("HHmmss");
+                var part = new PartType()
+                {
+                    Name = partName
+                };
+                ctxt.Parts.Add(part);
+                ctxt.SaveChanges();
+
+                var getPart = ctxt.Parts.Single(
+                    p => p.Name == partName);
+                Assert.Equal(getPart.Name, part.Name);
+
+                ctxt.Parts.Remove(getPart);
+                ctxt.SaveChanges();
+
+                getPart = ctxt.Parts.FirstOrDefault(
+                    p => p.Name == partName);
+                Assert.Null(getPart);
+            }
+        }
+    }
+}
+
